ca6a0ed41aec407b1b7b020a6e5e22b463b53857
DanielDanielsson <danielssondaniel94@gmail.com>
2023-09-03 08:36:22 +0200

    feat: add recommended config

---
+++ src/config/recommended.ts
@@ -0,0 +1,9 @@
+export default {
+  plugins: ["typescript-sort"],
+  rules: {
+    "typescript-sort/sort-interface": "error",
+    "typescript-sort/sort-type": "error",
+    "typescript-sort/sort-enum": "error",
+    "typescript-sort/sort-arrowfunc-object-params": "error",
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { sortInterface } from "./rules/sortInterface.ts";
 import { sortType } from "./rules/sortType.ts";
 import { sortEnum } from "./rules/sortEnum.ts";
 import { sortArrowFuncObjectParams } from "./rules/sortArrowFuncObjectParams.ts";
+import recommended from "./config/recommended";
 
 const rules = {
   "sort-interface": sortInterface,
@@ -12,6 +13,9 @@ const rules = {
 
 const config = {
   rules,
+  configs: {
+    recommended,
+  },
 };
 
 export default config;
